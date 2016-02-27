cd3a10ddbf44c6b03d9117f5d889366fb5cefe3c
Ilan BUCHWALD <ibuchwal@e2r2p3.42.fr>
2016-02-28 00:38:27 +0100

    personal

--- foo/bar/urls.py
+++ foo/bar/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import IndexView, GraphView
+from .views import IndexView, GraphView, PersonalView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^graph/$', GraphView.as_view(), name='graph'),
+    url(r'^(?P<slug>[-\w]+)/$', PersonalView.as_view(), name='personal'),
 ]
--- foo/bar/views.py
+++ foo/bar/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.views.generic import TemplateView
-
-from .models import Account
+from django.core import serializers
+from .models import Client, Account
 
 
 class IndexView(TemplateView):
@@ -13,6 +13,30 @@ class GraphView(TemplateView):
 
 	def get_context_data(self, **kwargs):
 		context = super(GraphView, self).get_context_data(**kwargs)
-		context['money'] = get_object_or_404(Account, account_id='CL42Co3')
+		client = Client.objects.get(client_id=36)
+		# context['money'] = get_object_or_404(Account, account_id='CL42Co3')
+		json_serializer = serializers.get_serializer("json")()
+		context['accounts'] = json_serializer.serialize(Account.objects.all().filter(client_id=client), ensure_ascii=False)
+		# accounts = Account.objects.all().filter(client_id=client)
+		# print accounts
+		# for a in accounts:
+			# print a.client_id
 
+
+		return context
+
+
+class PersonalView(TemplateView):
+	template_name = 'bar/personal.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(PersonalView, self).get_context_data(**kwargs)
+		client = Client.objects.get(client_id=kwargs['slug'])
+		# context['money'] = get_object_or_404(Account, account_id='CL42Co3')
+		json_serializer = serializers.get_serializer("json")()
+		context['accounts'] = json_serializer.serialize(Account.objects.all().filter(client_id=client), ensure_ascii=False)
+		# accounts = Account.objects.all().filter(client_id=client)
+		# print accounts
+		# for a in accounts:
+			# print a.client_id
 		return context
