b4f509e323a9df5bc8d6a18e06185f48e5a63e57
Ilan BUCHWALD <ibuchwal@e2r2p3.42.fr>
2016-02-27 12:39:50 +0100

    foundation

--- foo/foo/settings.py
+++ foo/foo/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'suit',
-    'bar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bar',
+    'foundation'
 ]
 
 MIDDLEWARE_CLASSES = [
